bb6af66dde2ca25d80b1e396d07671691d9a1eaa
Michael Douglass <mikedoug@mikedoug.net>
2019-12-07 00:33:54 -0600

    Day 7!
    I used Day 5's computer with a bug fix and enhanced to permit the
    program entering a WAIT state for further input.  I updated day
    5's copy as well so you can easily see the differences; it still works
    for day 5's code.

---
+++ 2019/07/prog.py
@@ -0,0 +1,84 @@
+import computer
+
+with open("input.txt", "r") as f:
+    software = list(map(lambda x: int(x),f.readline().rstrip().split(",")))
+
+outputs = []
+for a in range(5):
+    program = computer.Computer(software)
+    results = program.execute([a, 0])
+    outputA = results[0]
+
+    for b in range(5):
+        if a == b:
+            continue
+        program = computer.Computer(software)
+        results = program.execute([b, outputA])
+        outputB = results[0]
+
+        for c in range(5):
+            if c in [a,b]:
+                continue
+            program = computer.Computer(software)
+            results = program.execute([c, outputB])
+            outputC = results[0]
+
+            for d in range(5):
+                if d in [a, b, c]:
+                    continue
+                program = computer.Computer(software)
+                results = program.execute([d, outputC])
+                outputD = results[0]
+
+                for e in range(5):
+                    if e in [a, b, c, d]:
+                        continue
+                    program = computer.Computer(software)
+                    results = program.execute([e, outputD])
+                    # print(f"{a} {b} {c} {d} {e} -- {outputA} {outputB} {outputC} {outputD} {results[0]}")
+                    outputs.append(results[0])
+
+print (f"Maximum output part one: {max(outputs)}")
+if max(outputs) != 929800:
+    print("^^^^^^ FAIL ^^^^^^")
+
+
+def feedback_loop(phases):
+    programs = []
+    for phase in phases:
+        program = computer.Computer(software)
+        program.safe_execute([phase])
+
+        programs.append(program)
+
+    input = 0
+    while True:
+        for program in programs:
+            program.safe_execute([input])
+            input = program.outputs[-1]
+
+        if programs[-1].state == computer.State.HALT:
+            return input
+
+outputs = []
+for a in range(5,10):
+    for b in range(5,10):
+        if a == b:
+            continue
+        for c in range(5,10):
+            if c in [a,b]:
+                continue
+            for d in range(5,10):
+                if d in [a, b, c]:
+                    continue
+                for e in range(5,10):
+                    if e in [a, b, c, d]:
+                        continue
+                    outputs.append(feedback_loop([a,b,c,d,e]))
+
+print (f"Maximum output part two: {max(outputs)}")
+if max(outputs) != 15432220:
+    print("^^^^^^ FAIL ^^^^^^")
+
+
+
