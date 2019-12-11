d1ef58460af88910959ac28bdda5c24ffb23bf95
Michael Douglass <mikedoug@mikedoug.net>
2019-12-10 20:51:13 -0600

    Simplify day 10

--- 2019/10/prog.py
+++ 2019/10/prog.py
@@ -1,70 +1,28 @@
 import math
 
-# Given three colinear points p, q, r, the function checks if 
-# point q lies on line segment 'pr' 
-def onSegment(p, q, r):
-    return q[0] <= max(p[0], r[0]) and q[0] >= min(p[0], r[0]) and q[1] <= max(p[1], r[1]) and q[1] >= min(p[1], r[1])
-
-# To find orientation of ordered triplet (p, q, r). 
-# The function returns following values 
-# 0 --> p, q and r are colinear 
-# 1 --> Clockwise 
-# 2 --> Counterclockwise 
-def orientation(p, q, r):
-    # See https://www.geeksforgeeks.org/orientation-3-ordered-points/ 
-    # for details of below formula. 
-    val = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
-  
-    if val == 0:
-        return 0  # colinear 
-  
-    return 1 if (val > 0) else 2 # clock or counterclock wise 
-
-
-def find_visible(fieldset, point):
-    visibleset = list()
+def get_angles(fieldset, point, sort=False):
+    angles = {}
     for dest in fieldset:
         if point == dest:
             continue
 
-        visible = True
-        for test in fieldset:
-            if test == point or test == dest:
-                continue
+        angle = math.degrees(math.atan2(dest[1] - point[1], dest[0] - point[0])) + 90
+        angle += 360 if angle < 0 else 0
 
-            if orientation(point, test, dest) == 0:
-                if onSegment(point, test, dest):
-                    visible = False
-                    break
+        if angle not in angles:
+            angles[angle] = []
+        angles[angle].append(dest)
 
-        if visible:
-            visibleset.append(dest)
+    if sort:
+        for angle in angles:
+            angles[angle] = list(sorted(angles[angle], key=lambda x: line_len(point, x)))
 
-    return visibleset
+    return angles
 
-
-def destroy(fieldset, station):
-    fieldset = list(fieldset)
-
-    start = 1
-    iteration = 1
-    while len(fieldset) > 1:
-        print(f'PASS: {iteration}')
-        visiblelist = find_visible(fieldset, station)
-
-        angles = {}
-        for point in visiblelist:
-            angle = math.degrees(math.atan2(point[1] - station[1], point[0] - station[0])) + 90
-            angle = angle + 360 if angle < 0 else angle
-            angles[point] = angle # if angle != 360 else 0.0
-
-            fieldset.remove(point)
-
-        for i,(point,angle) in enumerate(sorted(angles.items(), key=lambda x: x[1])):
-            print(f'  {i+start:4}: {point} angle = {angle}')
-        
-        start += len(visiblelist)
-        iteration += 1
+def line_len(a, b):
+    x = a[0] - b[0]
+    y = a[1] - b[1]
+    return math.sqrt(x*x + y*y)
 
 
 def main():
@@ -81,13 +39,27 @@ def main():
             if row[x] != '.':
                 fieldset[(x,y)] = 0
 
-    for point in fieldset:
-        fieldset[point] = find_visible(fieldset, point)
+    # Step 1
+    for point in fieldset.keys():
+        fieldset[point] = len(get_angles(fieldset, point))
 
-    station, visiblelist = max(fieldset.items(), key=lambda x: len(x[1]))
-    print(f'Total asteroids: {len(fieldset)}')
-    print(f'Step One: {station} length = {len(visiblelist)}')
+    (station, count) = max(fieldset.items(), key=lambda x: x[1])
+    print(f'Step One: {station} length = {count}')
     print()
 
-    destroy(fieldset, station)
+    # Step 2
+    angles = get_angles(fieldset, station, sort=True)
+
+    destroyed = []
+    while len(angles) > 0:
+        angle_keys = list(sorted(angles.keys()))
+        for angle in angle_keys:
+            point = angles[angle].pop(0)
+            if len(angles[angle]) == 0:
+                del angles[angle]
+            destroyed.append((point, angle))
+            # print(f'{len(destroyed):5}: {point} @ {angle}')
+
+    print(f'200th destroyed asteroid: {destroyed[199]}')
+
 main()
