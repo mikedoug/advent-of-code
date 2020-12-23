132a455efaf2f1e4677844d680e8192b1a9a2a96
Michael Douglass <mikedoug@mikedoug.net>
2020-12-23 02:11:24 -0600

    updates

--- 2020/20/Program.cs
+++ 2020/20/Program.cs
@@ -7,6 +7,28 @@ using System.Text.RegularExpressions;
 
 namespace _20
 {
+    public static class RegexExtension {
+        public static IEnumerable<Match> MatchOverlap(this Regex regex, string input) {
+            Match matchObj = regex.Match(input);
+            yield return matchObj;
+            while (matchObj.Success) {
+                matchObj = regex.Match(input, matchObj.Index + 1); 
+                yield return matchObj;
+            }
+        }
+
+        public static IEnumerable<int> AllIndexes(this String input, char find) {
+            int offset = input.IndexOf(find);
+            while(offset != -1) {
+                yield return offset;
+                if (offset + 1 < input.Length) {
+                    offset = input.IndexOf(find, offset + 1);
+                } else {
+                    offset = -1;
+                }
+            }
+        }
+    }
     enum SIDES {
         TOP = 0,
         RIGHT,
@@ -14,13 +36,25 @@ namespace _20
         LEFT
     }
 
+    class Orientation {
+        public int[] Sides;
+        public bool Flip;
+        public int Rotations;
+
+        public Orientation(int[] sides, bool flip, int rotations) {
+            Sides = sides;
+            Flip = flip;
+            Rotations = rotations;
+        }
+    }
+
     class Piece
     {
         public int Number {get; init;}
-        public List<int[]> Orientations;
+        public List<Orientation> Orientations;
         public int[] DistinctValues;
 
-        public int[] Placement { get; private set; } = null;
+        public Orientation Placement { get; set; } = null;
 
         public int[] SideValues { get; private set; } = null;
         public Piece[] Adjacent { get; private set; } = null;
@@ -30,21 +64,21 @@ namespace _20
         public bool IsCorner { get {return EdgeCount() == 2; }}
         public bool IsEdge { get {return EdgeCount() == 1; }}
 
+        public List<string> Lines;
+
         public Piece(int number, List<string> lines) {
             Number = number;
+            Lines = lines;
 
             SideValues = new int[4];
             Adjacent = new Piece[4];
 
-            Orientations = new List<int[]>();
+            Orientations = new List<Orientation>();
             var normalOrientation = new int[4];
             var flippedSides = new int[4];
 
             // Top
             var topString = lines[0].Replace('#', '1').Replace('.','0');
-            Console.WriteLine(topString);
-            Console.WriteLine(topString.Reverse().ToArray());
-            Console.WriteLine("-------");
             var A = Convert.ToInt32(topString, 2);
             var Ai = Convert.ToInt32(new string(topString.Reverse().ToArray()), 2);
 
@@ -63,34 +97,64 @@ namespace _20
             var D = Convert.ToInt32(leftString, 2);
             var Di = Convert.ToInt32(new string(leftString.Reverse().ToArray()), 2);
 
-            Orientations.Add(new int[4] { A,  B,  C,  D  }); // Natural
-            Orientations.Add(new int[4] { Di, A,  Bi, C  }); // Natural + Rotate 1
-            Orientations.Add(new int[4] { Ci, Di, Ai, Bi }); // Natural + Rotate 2
-            Orientations.Add(new int[4] { B,  Ci, D,  Ai }); // Natural + Rotate 3
+            Orientations.Add(new Orientation(new int[4] { A,  B,  C,  D  }, false, 0)); // Natural
+            Orientations.Add(new Orientation(new int[4] { Di, A,  Bi, C  }, false, 1)); // Natural + Rotate 1
+            Orientations.Add(new Orientation(new int[4] { Ci, Di, Ai, Bi }, false, 2)); // Natural + Rotate 2
+            Orientations.Add(new Orientation(new int[4] { B,  Ci, D,  Ai }, false, 3)); // Natural + Rotate 3
 
-            Orientations.Add(new int[4] { Ai, D,  Ci, B  }); // Flipped Vertical
-            Orientations.Add(new int[4] { Bi, Ai, Di, Ci }); // Flipped Vertical + Rotate 1
-            Orientations.Add(new int[4] { C,  Bi, A,  Di }); // Flipped Vertical + Rotate 2
-            Orientations.Add(new int[4] { D,  C,  B,  A  }); // Flipped Vertical + Rotate 3
+            Orientations.Add(new Orientation(new int[4] { Ai, D,  Ci, B  }, true, 0)); // Flipped Vertical
+            Orientations.Add(new Orientation(new int[4] { Bi, Ai, Di, Ci }, true, 1)); // Flipped Vertical + Rotate 1
+            Orientations.Add(new Orientation(new int[4] { C,  Bi, A,  Di }, true, 2)); // Flipped Vertical + Rotate 2
+            Orientations.Add(new Orientation(new int[4] { D,  C,  B,  A  }, true, 3)); // Flipped Vertical + Rotate 3
 
             DistinctValues = new int[8] { A, B, C, D, Ai, Bi, Ci, Di };
-        }
-
-        public void OrientFirstPiece() {
-            for (int i = 0; i < 4; i++) {
-                Orient(Orientations[0][i], i);
-            }
         }
 
         // Sets edges to -1
         public void SquashEdges(Dictionary<int, List<Piece>> edgeMapping) {
             foreach (var orientation in Orientations) {
                 for (var i = 0; i < 4; i++) {
-                    if (edgeMapping[orientation[i]].Count == 1) {
-                        orientation[i] = -1;
+                    if (edgeMapping[orientation.Sides[i]].Count == 1) {
+                        orientation.Sides[i] = -1;
                     }
                 }
             }
+        }
+
+        public List<string> getLines() {
+
+            var useLines = Lines.Skip(1).Take(Lines.Count - 2);
+            if (Placement.Flip) {
+                useLines = useLines.Select(line => new String(line.Reverse().ToArray()));
+            }
+            useLines = useLines.Select(line => new String(line.Skip(1).Take(line.Length-2).ToArray()));
+            var size = useLines.First().Length;
+
+            if (Placement.Rotations == 1) {
+                var finalLines = new List<string>();
+                for (var i = 0; i < size; i++) {
+                    finalLines.Add(useLines.Select(line => line[i]).Aggregate("", (a, p) => p+a));
+                }
+                return finalLines;
+            } else if (Placement.Rotations == 2) {
+                var finalLines = new List<string>();
+                for (var i = size - 1; i > 0; i--) {
+                    finalLines = useLines.Select(line => new String(line.Reverse().ToArray())).Reverse().ToList();
+                }
+                return finalLines;     
+            } else if (Placement.Rotations == 3) {
+                var finalLines = new List<string>();
+                for (var i = 0; i < size; i++) {
+                    finalLines.Add(useLines.Select(line => line[size-1-i]).Aggregate("", (a, p) => a+p));
+                }
+                return finalLines;
+            } else {
+                return useLines.ToList();
+            }
+        }
+
+        public int ImageSize() {
+            return Lines.Count - 2;
         }
 
         public void Orient(int value, int position) {
@@ -109,19 +173,20 @@ namespace _20
             Console.WriteLine($"ORIENT VALUES: {this} {String.Join(", ",SideValues)}");
 
             // SHORT CIRCUIT AN ALREADY MATCH
-            if (Placement != null && Placement[position] == value) {
+            if (Placement != null && Placement.Sides[position] == value) {
                 return;
             }
 
             var matchingOrientations = Orientations
-                .Where(orientation => Enumerable.All(Enumerable.Range(0,4), i => SideValues[i] == 0 || orientation.Contains(SideValues[i])));
+                .Where(orientation => Enumerable.All(Enumerable.Range(0,4), i => SideValues[i] == 0 || orientation.Sides.Contains(SideValues[i])));
             if (matchingOrientations.Count() == 0) {
                 throw new Exception("Could not find an orientation with all of the required values");
             }
 
-            foreach (var matchingOrientation in matchingOrientations) {
-                Console.WriteLine($" -- Trying: {String.Join(", ",matchingOrientation)}");
-                if (Enumerable.All(Enumerable.Range(0,4), i => SideValues[i] == 0 || matchingOrientation[i] == SideValues[i])) {
+            foreach (var matchingOrientation in matchingOrientations.Reverse()) {
+            // foreach (var matchingOrientation in matchingOrientations) {
+                Console.WriteLine($" -- Trying: {String.Join(", ",matchingOrientation.Sides)}");
+                if (Enumerable.All(Enumerable.Range(0,4), i => SideValues[i] == 0 || matchingOrientation.Sides[i] == SideValues[i])) {
                     // We found the right mix.
                     Placement = matchingOrientation;
                     Console.WriteLine($" -- Result: {String.Join(", ",Placement)}");
@@ -136,11 +201,11 @@ namespace _20
             if (this.Placement == null) {
                 var builder = new StringBuilder();
                 foreach (var orientation in Orientations) {
-                    builder.Append($" [{String.Join(", ", orientation)}]");
+                    builder.Append($" [{String.Join(", ", orientation.Sides)}]");
                 }
                 return $"{Number}:{builder.ToString()}";
             } else {
-                return $"{Number}: [{String.Join(", ", Placement)}] [{String.Join(", ", Adjacent.Select(piece => piece == null ? 0 : piece.Number))}]";
+                return $"{Number}: [{String.Join(", ", Placement.Sides)}] [{String.Join(", ", Adjacent.Select(piece => piece == null ? 0 : piece.Number))}] [{Placement.Flip} {Placement.Rotations}]";
             }
         }
 
@@ -167,11 +232,7 @@ namespace _20
         }
 
         public int EdgeCount() {
-            // if (Placement != null) {
-            //     return Placement.Where(i => edgeMapping[i].Count == 1).ToList();    
-            // }
-            // return Orientations[0].Where(i => edgeMapping[i].Count == 1).ToList();
-            return Orientations[0].Count(i => i == -1);
+            return Orientations[0].Sides.Count(i => i == -1);
         }
     }
 
@@ -183,7 +244,6 @@ namespace _20
             var pieces = Piece.ParseInput(System.IO.File.ReadAllLines("input.txt"));
             Console.WriteLine($"Total Pieces: {pieces.Count}");
             foreach (var piece in pieces) {
-                // piece.Print();
                 foreach (var value in piece.DistinctValues) {
                     if (!mapping.ContainsKey(value)) {
                         mapping[value] = new List<Piece>();
@@ -191,11 +251,6 @@ namespace _20
                     mapping[value].Add(piece);
                 }
             }
-
-            // // Debug output to show that we have exactly the right number of connections...
-            // foreach (var entry in mapping) {
-            //     Console.WriteLine($"COUNT {entry.Value.Count}: {entry.Key}");
-            // }
 
             foreach (var piece in pieces) {
                 piece.SquashEdges(mapping);
@@ -205,31 +260,26 @@ namespace _20
             var agg = 1L;
             foreach (var piece in cornerPieces) {
                 Console.WriteLine(piece);
-                // foreach(var i in piece.Orientations[0].Where(i => mapping[i].Count == 1)) {
-                //     Console.WriteLine($"   {i}");
-                // }
                 agg *= piece.Number;
             }
             Console.WriteLine($"Value: {agg}");
 
-            var bottomLeftPiece = cornerPieces.First();
+            var bottomLeftPiece = cornerPieces.Skip(1).First();
             var lastPiece = bottomLeftPiece;
             lastPiece.Orient(-1,2);
             lastPiece.Orient(-1,3);
 
-            // // lastPiece.Orient(lastEdgeValues[0], 2);
-            // // lastPiece.Orient(lastEdgeValues[1], 3);
             Console.WriteLine($"-----------Placed: {lastPiece}");
 
             // Build the frame
             for(int walkDir = 0; walkDir < 4; ) {
-                var nextPiece = mapping[lastPiece.Placement[walkDir]].Where(item => item != lastPiece && (item.Adjacent == null || item.Adjacent[(walkDir+2)%4] == null)).FirstOrDefault();
+                var nextPiece = mapping[lastPiece.Placement.Sides[walkDir]].Where(item => item != lastPiece && (item.Adjacent == null || item.Adjacent[(walkDir+2)%4] == null)).FirstOrDefault();
                 Console.WriteLine($"  Considering: {nextPiece}");
 
                 lastPiece.Adjacent[walkDir] = nextPiece;
                 nextPiece.Adjacent[(walkDir + 2)%4] = lastPiece;
 
-                nextPiece.Orient(lastPiece.Placement[walkDir], (walkDir+2)%4);
+                nextPiece.Orient(lastPiece.Placement.Sides[walkDir], (walkDir+2)%4);
                 nextPiece.Orient(-1, (walkDir+3)%4);
                 if (nextPiece.EdgeCount() == 2) {
                     nextPiece.Orient(-1, walkDir);
@@ -244,20 +294,20 @@ namespace _20
             Console.WriteLine("FRAME DONE");
 
             // Fill in the rows
-            var lastRowPiece = cornerPieces.First();
+            var lastRowPiece = bottomLeftPiece;
             for (var rowHead = lastRowPiece.Adjacent[0]; ! rowHead.IsCorner; lastRowPiece = rowHead, rowHead = rowHead.Adjacent[0]) {
                 Console.WriteLine("Starting Row");
                 Console.WriteLine(rowHead);
                 for (var piece = rowHead; ; piece = piece.Adjacent[1]) {
                     lastRowPiece = lastRowPiece.Adjacent[1];
 
-                    var nextPiece = mapping[piece.Placement[1]].Where(item => item != piece && (item.Adjacent == null || item.Adjacent[3] == null)).FirstOrDefault();
+                    var nextPiece = mapping[piece.Placement.Sides[1]].Where(item => item != piece && (item.Adjacent == null || item.Adjacent[3] == null)).FirstOrDefault();
                     Console.WriteLine($"PLACING: {nextPiece}");
                     Console.WriteLine($"LASTROW: {lastRowPiece}");
                     piece.Adjacent[1] = nextPiece;
                     nextPiece.Adjacent[3] = piece;
-                    nextPiece.Orient(piece.Placement[1], 3);
-                    nextPiece.Orient(lastRowPiece.Placement[0], 2);
+                    nextPiece.Orient(piece.Placement.Sides[1], 3);
+                    nextPiece.Orient(lastRowPiece.Placement.Sides[0], 2);
                     Console.WriteLine($"-----------Placed: {nextPiece}");
                     Console.WriteLine("");
 
@@ -274,8 +324,89 @@ namespace _20
                 rowHeads.Insert(0, piece);
             }
 
-            foreach (var rowHead in rowHeads) {
+            var builders = new StringBuilder[bottomLeftPiece.ImageSize()];
 
+            var thePuzzle = new List<string>();
+            Console.WriteLine("");
+            foreach (var rowHead in rowHeads) {
+                for(int i = 0; i < bottomLeftPiece.ImageSize(); i++) {
+                    builders[i] = new StringBuilder();
+                }
+
+                Console.WriteLine("-------");
+                for (var piece = rowHead; piece != null; piece = piece.Adjacent[1]) {
+                    Console.WriteLine(piece);
+                    var pieceLines = piece.getLines();
+                    for(int i = 0; i < bottomLeftPiece.ImageSize(); i++) {
+                        Console.WriteLine($"   {pieceLines[i]}");
+                        builders[i].Append(pieceLines[i]);
+                    }
+                }
+
+                /// Do something with the builders here -- these rows are done
+                foreach (var builder in builders) {
+                    thePuzzle.Add(builder.ToString());
+                }
+            }
+
+            Console.WriteLine("------- the puzzle -------");
+            foreach (var line in thePuzzle) {
+                Console.WriteLine(line);
+            }
+
+            var snake1 = new Regex(@".{18}#.");
+            var snake2 = new Regex(@"^#....##....##....###$");
+            var snake3 = new Regex(@"^.#..#..#..#..#..#...$");
+
+            bool foundSnake = false;
+            for(int i = 0; i < thePuzzle.Count - 2; i++) {
+                var matches = snake1.MatchOverlap(thePuzzle[i]);
+                foreach (Match match in matches) {
+                    // Console.WriteLine($"MATCH START: {i} {match.Index}");
+
+                    if (snake2.IsMatch(thePuzzle[i+1].Substring(match.Index, 20))) {
+                        // Console.WriteLine("  -- Line 2 matches");
+                        if (snake3.IsMatch(thePuzzle[i+2].Substring(match.Index, 20))) {
+                            // Console.WriteLine("  -- Line 3 matches");
+                            Console.WriteLine($"MATCH: {i} {match.Index}");
+                            Console.WriteLine(thePuzzle[i].Substring(match.Index, 20));
+                            Console.WriteLine(thePuzzle[i+1].Substring(match.Index, 20));
+                            Console.WriteLine(thePuzzle[i+2].Substring(match.Index, 20));
+
+                            foundSnake = true;
+
+                            var x = match.Index;
+
+                            var lineChars = thePuzzle[i].ToArray();
+                            lineChars[x+18] = 'O';
+                            thePuzzle[i] = new String(lineChars);
+
+                            lineChars = thePuzzle[i+1].ToArray();
+                            foreach( var offset in "#....##....##....###".AllIndexes('#')) {
+                                lineChars[x+offset] = 'O';
+                            }
+                            thePuzzle[i+1] = new String(lineChars);
+
+                            lineChars = thePuzzle[i+2].ToArray();
+                            foreach( var offset in ".#..#..#..#..#..#...".AllIndexes('#')) {
+                                lineChars[x+offset] = 'O';
+                            }
+                            thePuzzle[i+2] = new String(lineChars);
+
+
+                        }
+                    }
+                }
+            }
+
+            if (foundSnake) {
+                Console.WriteLine("------- the puzzle -------");
+                foreach (var line in thePuzzle) {
+                    Console.WriteLine(line);
+                }
+
+                var result = thePuzzle.Sum(line => line.Count(c => c == '#'));
+                Console.WriteLine($"Result: {result}");
             }
         }
     }
---
+++ 2020/23/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace _23
+{
+    static class Extensions {
+        public static IEnumerable<long> LongRange(long start, long count)
+        {
+            var limit = start + count;
+
+            while (start < limit)
+            {
+                yield return start;
+                start++;
+            }
+        }
+    }
+
+    class Node {
+        public int Value {get; init;}
+        public Node Next;
+
+        public Node(int value) {
+            Value = value;
+        }
+
+        public override string ToString() {
+            return $"{Value}";
+        }
+
+        public IEnumerable<Node> Walk() {
+            yield return this;
+            for (Node ptr = Next; ptr != this; ptr = ptr.Next) {
+                yield return ptr;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cupsList = "167248359".Select(c => Int32.Parse($"{c}")).Union(Enumerable.Range(10, 1_000_000-9));
+            var cups = cupsList.Select(value => new Node(value)).ToList();
+            var map = cups.ToDictionary(item => item.Value);
+
+            for (var i = 0; i < cups.Count; i++) {
+                cups[i].Next = cups[(i+1) % cups.Count];
+            }
+
+            var current = cups[0];
+            var largest = cups.Max(item => item.Value);
+            
+            Console.WriteLine($"{cups.Count}");
+
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            // for(long move = 1L; move <= 10L; move++) {
+            for(long move = 1L; move <= 10_000_000L; move++) {
+                // Console.WriteLine($"-- move {move}");
+
+                // Console.WriteLine($"cups: ({current}) {String.Join("  ", map[1].Walk())}");
+
+                var removed1 = current.Next;
+                var removed3 = removed1.Next.Next;
+                // Console.WriteLine($"pick up: {removed1}, {removed1.Next}, {removed3}");
+
+                // Rewire the head of our cut
+                current.Next = removed3.Next;
+
+                // Rewire the tail of our cut
+                removed3.Next = null;
+                
+                var value = current.Value - 1;
+                if (value <= 0) {
+                    value = largest;
+                }                
+                Node choosen = null;
+                while(choosen == null) {
+                    if (value != removed1.Value && value != removed1.Next.Value && value != removed3.Value) {
+                        choosen = map[value];
+                    } else {
+                        value--;
+                        if (value <= 0) {
+                            value = largest;
+                        }
+                    }
+                }
+
+                // Console.WriteLine($"destination: {value}");
+                // Console.WriteLine("");
+                // cups = cups.Take(index+1).Union(removed).Union(cups.Skip(index+1)).Union(new int[]{current}).ToList();
+                
+                // rewire it back in
+                removed3.Next = choosen.Next;
+                choosen.Next = removed1;
+                
+                current = current.Next;
+            }
+            timer.Stop();
+            Console.WriteLine($"-- Elapsed: {timer.Elapsed}");
+
+            // Console.WriteLine($"cups: {String.Join("  ", map[1].Walk())}");
+            var one = map[1];
+            // for(var node = one.Next; node != one; node = node.Next) {
+            //     Console.WriteLine(node);
+            // }
+            Console.WriteLine($"Cups-to-right: {one.Next} {one.Next.Next}");
+            Console.WriteLine($"Product: {(long)one.Next.Value * (long)one.Next.Next.Value}");
+        }
+    }
+}
